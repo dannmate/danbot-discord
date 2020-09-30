80ce3cd6908b5560fb92ebbe0118b7a4518a6ee2
dannmate <dpetrolito@gmail.com>
2020-09-30 11:58:27 -0700

    added websocket functionality

--- index.js
+++ index.js
@@ -1,43 +1,74 @@
 require('dotenv').config()
 const Discord = require('discord.js');
+var io = require('socket.io-client')(process.env.SOCKET_SERVER_URL);
+
+io.on('connect', function(){});
+
+//this triggers when a someone first connects to the socket server, send them back current data.
+io.on('freshConnection', async function(msg){
+    console.log(msg);
+    const memberCollection = await getUsersOnlineStatus(client);
+     console.log(memberCollection);
+    io.emit('freshConnectionData', memberCollection);
+  });
 
 const client = new Discord.Client();
-
 client.once('ready', () =>{
     console.log('danbot is online');
 });
 
+//discord event that triggers everytime someone connects/disconnects/changes a voice channel.
 client.on('voiceStateUpdate', async (oldMember, newMember) => {
-    let newUserChannel = newMember.voiceChannel;
-    let oldUserChannel = oldMember.voiceChannel;
-    //console.log(oldMember);
+    const memberCollection = await getUsersOnlineStatus(client);
 
-    //console.log(client.channels);
-    //console.log(client.channels.cache.get("693620288910524416"));
-    //693620288910524416
-    const channel = client.channels.cache.get("693620288910524416");
-    for (const [memberID, member] of channel.members) {
-        console.log(member.user.username);
-      }
-  console.log(Object.keys(channel.members).length);
-    if(oldUserChannel === undefined && newUserChannel !== undefined) {
-        //console.log(newMember);
-       // User Joins a voice channel
-  
-    } else if(newUserChannel === undefined){
-        //console.log(oldMember);
-      // User leaves a voice channel
-  
-    }
+    io.emit('memberStatusChange', memberCollection);
+    console.log(memberCollection);
   });
 
-  client.on('message', message =>{
-    console.log(message.content);
+  async function getUsersOnlineStatus(cli) {
+    const channels = await cli.channels.cache;
+    let petrol = false;
+    let waldy = false;
+    let cashen = false;
+    let marly = false;
+    let kenzo = false;
+    let darbo = false;
+    let jack = false;
+    let nips = false;
+    let sheena = false;
 
-    if (message.content == 'test'){
-        message.channel.send('test1');
+    //get all voice channels from the discord server and check who's in a voice channel.
+    for (const channel of channels) {
+       // console.log(channel);
+        if (channel[1].type == 'voice') {
+            //console.log(channel[1].name);
+            const members = await client.channels.cache.get(channel[0]).members;
+            for (const [memberID, member] of members) {
+                console.log(member.user.username);
+                if (member.user.username == 'dannmate') { petrol = true;} 
+                if (member.user.username == 'Waldy') { waldy = true;} 
+                if (member.user.username == 'SammySpaceCadet') { cashen = true;} 
+                if (member.user.username == 'marlymarc') { marly = true;}
+                if (member.user.username == 'Kenzo') { kenzo = true;}
+                if (member.user.username == 'Darbsquire') { darbo = true;}
+                if (member.user.username == 'KingofFairies') { jack = true;}
+                if (member.user.username == 'Nipples') { nips = true;}
+                if (member.user.username == 'Sheena') { sheena = true;}
+            }
+        }
     }
-  });
+    
+    return {"petrol": petrol,
+            "waldy": waldy,
+            "cashen": cashen,
+            "marly": marly,
+            "kenzo": kenzo,
+            "darbo": darbo,
+            "jack": jack,
+            "nips": nips,
+            "sheena": sheena};
 
 
-client.login(process.env.MONGODB_CONNECTION_STRING);
+}
+
+client.login(process.env.DISCORD_TOKEN);
